3a6584dc4501f9b23b7befebf0a5fb3a24f9e4fa
sslakkimsetty <sslakkimsetty@protonmail.com>
2019-11-09 13:30:32 -0500

    Interpolation supports bilinear and nearest neighbor

--- R/utils.R
+++ R/utils.R
@@ -29,7 +29,25 @@ overlay_images <- function(fixed, moving, alpha=0.25, new=FALSE) {
 
 
 
+.validate_interpolate <- function(img, coords, method) {
+    method <- match.arg(method, c("bilinear", "nearest_neighbor"))
+
+    img <- .valid_img(img)
+
+    x <- dim(img)[2]
+    y <- dim(img)[1]
+
+    if (coords[1] <= 0 | coords[1] > x |
+        coords[2] <= 0 | coords[2] > y) {
+        stop("Coords out of bounds", call.=FALSE)}
+
+    TRUE
+}
+
+
 interpolate <- function(img, coords=c(h=1,w=1), method="bilinear") {
+    .valid <- .validate_interpolate(img, coords, method)
+
     h <- coords[[1]]
     w <- coords[[2]]
 
@@ -38,14 +56,26 @@ interpolate <- function(img, coords=c(h=1,w=1), method="bilinear") {
     w1 <- floor(w)
     w2 <- ceiling(w)
 
-    int <- c()
-    v <- matrix(c(h1,w1, h1,w2, h2,w1, h2,w2), ncol=2, byrow=TRUE)
-    d <- apply(v, 1, function(x) {
-        int <<- c(int, img[x[1], x[2]])
-        dist(matrix(c(x, coords), ncol=2, byrow=TRUE), method="euclidean")
-    })
+    neighbors <- matrix(c(h1,w1, h1,w2, h2,w1, h2,w2), ncol=2, byrow=TRUE)
+    int <- apply(neighbors, 1, function(x) img[x[1], x[2]])
 
-    sum(int * d) / sum(d)
+    if (method == "nearest_neighbor") {
+        d <- apply(neighbors, 1, function(x) {
+            dist(matrix(c(x, coords), ncol=2, byrow=TRUE), method="euclidean")
+        })
+        # sum(int * d) / sum(d)
+        int[which.min(d)]
+
+    } else if (method == "bilinear") {
+        k1 <- (w2 - w) * int[1] + (w - w1) * int[2]
+        k2 <- (w2 - w) * int[3] + (w - w1) * int[4]
+        k <- (h2 - h) * k1 + (h - h1) * k2
+        k
+    }
+
 }
 
 .k <- matrix(1:4, nrow=2, byrow=TRUE)
+
+interpolate(.k, c(1.8, 1.75))
+
